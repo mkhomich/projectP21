57bf2218907ca5800cb568abe14e9179b693b2bd
Беляев <36142255@mail.ru>
2024-04-25 20:07:46 +0300

    Исправлен Student

--- src/main/java/ru/top/project/model/Student.java
+++ src/main/java/ru/top/project/model/Student.java
@@ -6,30 +6,31 @@ import java.util.List;
 import java.util.Objects;
 
 public class Student extends User {
-    List<Student> studentList = new ArrayList<>();
+    List<Course> courseList = new ArrayList<>();
 
     public Student(String userName, String userLogin, String userPassword, String userId) {
         super(userName, userLogin, userPassword, userId);
     }
 
-    public List<Student> getStudentList() {
-        return studentList;
+    public List<Course> getCourseList() {
+        return courseList;
     }
 
-    public void setStudentList(List<Student> studentList) {
-        this.studentList = studentList;
+    public void setCourseList(List<Course> courseList) {
+        this.courseList = courseList;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Student student)) return false;
+        if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
-        return Objects.equals(getStudentList(), student.getStudentList());
+        Student student = (Student) o;
+        return Objects.equals(courseList, student.courseList);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), getStudentList());
+        return Objects.hash(super.hashCode(), courseList);
     }
 }
