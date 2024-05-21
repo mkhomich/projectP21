ec6d31550e1971ca94224ff281bc4a511ed52ce3
supersonicacrobat <fedorvskiym2121@mail.ru>
2024-05-21 18:53:56 +0300

    HibernateUtil

---
+++ src/main/java/ru/top/project/util/HibernateUtil.java
@@ -0,0 +1,15 @@
+package ru.top.project.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtil {
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+
+    }
+
+}
